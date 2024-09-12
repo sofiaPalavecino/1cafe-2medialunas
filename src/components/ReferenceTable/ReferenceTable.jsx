340678b1966cb8d490228a70952c3bf73cee5330
sofiaPalavecino <sofiaor29@gmail.com>
2024-09-12 18:40:28 -0300

    reference table

---
+++ src/components/ReferenceTable/ReferenceTable.jsx
@@ -0,0 +1,27 @@
+import './ReferenceTable.scss'
+
+export default function ReferenceTable(){
+
+    return (
+        <div className="reference-table">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th className='title'>Referencia Agosto 2024</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className='-level-2'>
+                        <td>1350 ARS = 1 USD</td>
+                    </tr>
+                    <tr className='-text'>
+                        <td>Cambio dólar blue al 23/08/2024</td>
+                    </tr>
+                    <tr className='-text'>
+                        <td>Precios actualiza hasta el 23/8. No lo tome con literalidad, tómelo con un vasito de soda.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
