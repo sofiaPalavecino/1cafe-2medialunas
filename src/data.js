42981c8f228bf1b42ee593c834ae1d27c838e58d
sofiaPalavecino <sofiaor29@gmail.com>
2024-09-18 13:25:05 -0300

    mod data

--- src/data.js
+++ src/data.js
@@ -51,43 +51,625 @@ export default {
                 "name": "El Gordo Valor",
                 "price": 8500,
                 "usdPrice": 6.30,
-                "type": "cafe"
+                "type": "cafe",
+                "barrio": "Constitución"
             },
             {
                 "id": 2,
                 "name": "El Bar Británico",
                 "price": 3400,
                 "usdPrice": 2.51,
-                "type": "cafe"
+                "type": "cafe-notable",
+                "barrio": "San Telmo"
             },
             {
                 "id": 3,
                 "name": "Exactas UBA",
                 "price": 1200,
                 "usdPrice": 0.88,
-                "type": "university"
+                "type": "university",
+                "barrio": "Facultad",
+                "history": {
+                    "2023": {
+                        "price": 400,
+                        "usdPrice": 0.55
+                    },
+                    "2022": {
+                        "price": 150,
+                        "usdPrice": 0.51
+                    }
+                }
             },
             {
                 "id": 4,
                 "name": "Mostaza",
                 "price": 1500,
                 "usdPrice": 1.11,
-                "type": "fast-food"
+                "type": "fast-food",
+                "barrio": "Cadena"
             },
             {
                 "id": 5,
                 "name": "Café Marinez",
                 "price": 4150,
                 "usdPrice": 3.07,
-                "type": "cafe-chain"
+                "type": "cafe-chain",
+                "barrio": "Cadena"
             },
             {
                 "id": 6,
                 "name": "Le Pain Quotidien",
                 "price": 9800,
                 "usdPrice": 7.25,
-                "type": "cafe-chain"
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 7,
+                "name": "Mc Donald's",
+                "price": 1600,
+                "usdPrice": 1.18,
+                "type": "fast-food",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 8,
+                "name": "Burger King",
+                "price": 1700,
+                "usdPrice": 1.25,
+                "type": "fast-food",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 9,
+                "name": "Dandy",
+                "price": 1900,
+                "usdPrice": 1.40,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 10,
+                "name": "La Americana",
+                "price": 2900,
+                "usdPrice": 2.14,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 11,
+                "name": "Bar Portuario",
+                "price": 3000,
+                "usdPrice": 2.22,
+                "type": "cafe",
+                "barrio": "La Boca"
+            },
+            {
+                "id": 12,
+                "name": "San José",
+                "price": 3200,
+                "usdPrice": 2.37,
+                "type": "cafe",
+                "barrio": "Mataderos"
+            },
+            {
+                "id": 13,
+                "name": "El Santo",
+                "price": 3200,
+                "usdPrice": 2.37,
+                "type": "cafe",
+                "barrio": "Boedo"
+            },
+            {
+                "id": 14,
+                "name": "Lo de Carlitos",
+                "price": 3200,
+                "usdPrice": 2.37,
+                "type": "fast-food",
+                "barrio": "Cadena"
             }
-        ]
+            ,
+            {
+                "id": 15,
+                "name": "Hardvard",
+                "price": 3200,
+                "usdPrice": 2.37,
+                "type": "cafe",
+                "barrio": "Balvanera"
+            },
+            {
+                "id": 16,
+                "name": "Tolón",
+                "price": 3200,
+                "usdPrice": 2.37,
+                "type": "cafe",
+                "barrio": "Palermo"
+            },
+            {
+                "id": 17,
+                "name": "Faricci",
+                "price": 3300,
+                "usdPrice": 2.44,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 18,
+                "name": "Comet",
+                "price": 3500,
+                "usdPrice": 2.59,
+                "type": "cafe",
+                "barrio": "Monserrat"
+            },
+            {
+                "id": 19,
+                "name": "El Túnel",
+                "price": 3500,
+                "usdPrice": 2.59,
+                "type": "cafe",
+                "barrio": "La Paternal"
+            },
+            {
+                "id": 20,
+                "name": "Jinni's Coffee",
+                "price": 3500,
+                "usdPrice": 2.59,
+                "type": "cafe",
+                "barrio": "Floresta"
+            },
+            {
+                "id": 21,
+                "name": "Bracia",
+                "price": 3500,
+                "usdPrice": 2.59,
+                "type": "cafe",
+                "barrio": "Belgrano"
+            },
+            {
+                "id": 22,
+                "name": "El faro",
+                "price": 3600,
+                "usdPrice": 2.66,
+                "type": "cafe",
+                "barrio": "Constitución"
+            },
+            {
+                "id": 23,
+                "name": "Caffé del Doge",
+                "price": 3600,
+                "usdPrice": 2.66,
+                "type": "cafe",
+                "barrio": "Belgrano"
+            },
+            {
+                "id": 24,
+                "name": "Bell Mezz",
+                "price": 3600,
+                "usdPrice": 2.66,
+                "type": "cafe",
+                "barrio": "Almagro"
+            },
+            {
+                "id": 25,
+                "name": "Petit Liniers",
+                "price": 3700,
+                "usdPrice": 2.74,
+                "type": "cafe",
+                "barrio": "Liniers"
+            },
+            {
+                "id": 26,
+                "name": "Naranjo y Flor",
+                "price": 3700,
+                "usdPrice": 2.74,
+                "type": "cafe",
+                "barrio": "Palermo"
+            },
+            {
+                "id": 27,
+                "name": "San Bernardo",
+                "price": 3700,
+                "usdPrice": 2.74,
+                "type": "cafe-notable",
+                "barrio": "Villa Crespo"
+            },
+            {
+                "id": 28,
+                "name": "El Federal",
+                "price": 3800,
+                "usdPrice": 2.81,
+                "type": "cafe-notable",
+                "barrio": "San Telmo"
+            },
+            {
+                "id": 29,
+                "name": "La Poesía",
+                "price": 3800,
+                "usdPrice": 2.81,
+                "type": "cafe-notable",
+                "barrio": "San Telmo"
+            },
+            {
+                "id": 30,
+                "name": "Havanna",
+                "price": 3800,
+                "usdPrice": 2.81,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 31,
+                "name": "Cafe Margot",
+                "price": 3800,
+                "usdPrice": 2.81,
+                "type": "cafe",
+                "barrio": "San Telmo"
+            },
+            {
+                "id": 32,
+                "name": "Otero",
+                "price": 3900,
+                "usdPrice": 2.88,
+                "type": "cafe",
+                "barrio": "Caballito"
+            },
+            {
+                "id": 33,
+                "name": "Homero Manzi",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "cafe-notable",
+                "barrio": "Boedo"
+            },
+            {
+                "id": 34,
+                "name": "Varela Varelita",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "cafe-notable",
+                "barrio": "Palermo"
+            },
+            {
+                "id": 35,
+                "name": "Buffalo Café",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "cafe",
+                "barrio": "La Paternal"
+            },
+            {
+                "id": 36,
+                "name": "El Imperio de la Pizza",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "cafe",
+                "barrio": "Chacarita"
+            },
+            {
+                "id": 37,
+                "name": "UBA Derecho",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 38,
+                "name": "El Balón",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "cafe",
+                "barrio": "Barracas"
+            },
+            {
+                "id": 39,
+                "name": "Bonafide",
+                "price": 4100,
+                "usdPrice": 3.03,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 40,
+                "name": "Ritz",
+                "price": 4100,
+                "usdPrice": 3.03,
+                "type": "cafe",
+                "barrio": "Colegiales"
+            },
+            {
+                "id": 41,
+                "name": "Biblos",
+                "price": 4200,
+                "usdPrice": 3.11,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 42,
+                "name": "EL Progreso",
+                "price": 4200,
+                "usdPrice": 3.11,
+                "type": "cafe",
+                "barrio": "Barracas"
+            },
+            {
+                "id": 43,
+                "name": "Café Rio",
+                "price": 4200,
+                "usdPrice": 3.11,
+                "type": "cafe",
+                "barrio": "Almagro"
+            },
+            {
+                "id": 44,
+                "name": "Roma",
+                "price": 4300,
+                "usdPrice": 3.18,
+                "type": "cafe",
+                "barrio": "La Boca"
+            },
+            {
+                "id": 45,
+                "name": "El Viejo Buzón",
+                "price": 4300,
+                "usdPrice": 3.18,
+                "type": "cafe",
+                "barrio": "Caballito"
+            },
+            {
+                "id": 46,
+                "name": "Tarzán",
+                "price": 4400,
+                "usdPrice": 3.25,
+                "type": "cafe",
+                "barrio": "Balvanera"
+            },
+            {
+                "id": 47,
+                "name": "Negro",
+                "price": 4500,
+                "usdPrice": 3.33,
+                "type": "cafe",
+                "barrio": "Centro"
+            },
+            {
+                "id": 48,
+                "name": "Agrado",
+                "price": 4500,
+                "usdPrice": 3.33,
+                "type": "cafe",
+                "barrio": "Coghlan"
+            },
+            {
+                "id": 49,
+                "name": "Musel",
+                "price": 4800,
+                "usdPrice": 3.55,
+                "type": "cafe",
+                "barrio": "Constitución"
+            },
+            {
+                "id": 50,
+                "name": "Titola",
+                "price": 4800,
+                "usdPrice": 3.55,
+                "type": "cafe",
+                "barrio": "Colegiales"
+            },
+            {
+                "id": 51,
+                "name": "Los Galgos",
+                "price": 5100,
+                "usdPrice": 3.78,
+                "type": "cafe-notable",
+                "barrio": "San Nicolás"
+            },
+            {
+                "id": 52,
+                "name": "Doc Café",
+                "price": 5220,
+                "usdPrice": 3.87,
+                "type": "cafe",
+                "barrio": "Villa Crespo"
+            },
+            {
+                "id": 53,
+                "name": "Fiamma",
+                "price": 5300,
+                "usdPrice": 3.93,
+                "type": "cafe",
+                "barrio": "Flores"
+            },
+            {
+                "id": 54,
+                "name": "El Gato Negro",
+                "price": 5600,
+                "usdPrice": 4.14,
+                "type": "cafe-notable",
+                "barrio": "Centro"
+            },
+            {
+                "id": 55,
+                "name": "La Biela",
+                "price": 5600,
+                "usdPrice": 4.14,
+                "type": "cafe-notable",
+                "barrio": "Recoleta"
+            },
+            {
+                "id": 56,
+                "name": "Plaza Café",
+                "price": 6400,
+                "usdPrice": 4.74,
+                "type": "cafe-notable",
+                "barrio": "Caballito",
+                "history": {
+                    "2023": {
+                        "price": 910,
+                        "usdPrice": 3
+                    }
+                }
+            },
+            {
+                "id": 57,
+                "name": "La Ideal",
+                "price": 6800,
+                "usdPrice": 5.03,
+                "type": "cafe-notable",
+                "barrio": "Centro",
+                "history": {
+                    "2023": {
+                        "price": 3000,
+                        "usdPrice": 4.16
+                    }
+                }
+            },
+            {
+                "id": 58,
+                "name": "Tortoni",
+                "price": 6800,
+                "usdPrice": 5.03,
+                "type": "cafe-notable",
+                "barrio": "Monserrat"
+            },
+            {
+                "id": 59,
+                "name": "Petit Colón",
+                "price": 6900,
+                "usdPrice": 5.11,
+                "type": "cafe-notable",
+                "barrio": "Tribunales"
+            },
+            {
+                "id": 60,
+                "name": "Bilbo",
+                "price": 6900,
+                "usdPrice": 5.11,
+                "type": "cafe",
+                "barrio": "Villa Crespo"
+            },
+            {
+                "id": 61,
+                "name": "Croque Madame",
+                "price": 7500,
+                "usdPrice": 5.55,
+                "type": "cafe-chain",
+                "barrio": "Cadena"
+            },
+            {
+                "id": 62,
+                "name": "Psicología",
+                "price": 1500,
+                "usdPrice": 1.11,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 63,
+                "name": "Sociales",
+                "price": 1500,
+                "usdPrice": 1.11,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 64,
+                "name": "Farmacia y Bio.",
+                "price": 1600,
+                "usdPrice": 1.19,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 65,
+                "name": "Ingeniería",
+                "price": 1700,
+                "usdPrice": 1.26,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 66,
+                "name": "Filo y Letras",
+                "price": 1800,
+                "usdPrice": 1.33,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 67,
+                "name": "Economía",
+                "price": 1800,
+                "usdPrice": 1.33,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 68,
+                "name": "Agronomía",
+                "price": 2400,
+                "usdPrice": 1.78,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 69,
+                "name": "CBC M. de Oca",
+                "price": 2500,
+                "usdPrice": 1.85,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 70,
+                "name": "Medicina",
+                "price": 2500,
+                "usdPrice": 1.85,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 71,
+                "name": "UTN",
+                "price": 2500,
+                "usdPrice": 1.85,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 72,
+                "name": "FADU",
+                "price": 2800,
+                "usdPrice": 2.07,
+                "type": "university",
+                "barrio": "Facultad"
+            },
+            {
+                "id": 73,
+                "name": "Derecho",
+                "price": 4000,
+                "usdPrice": 2.96,
+                "type": "university",
+                "barrio": "Facultad"
+            }
+        ],
+        "currencies": {
+            "2024": {
+                "currency": 1350,
+                "cheapestBar": 3,
+                "mostExpensiveBar": 6
+            },
+            "2023": {
+                "currency": 720,
+                "cheapestBar": 3,
+                "mostExpensiveBar": 57
+            },
+            "2022": {
+                "currency": 290,
+                "cheapestBar": 3,
+                "mostExpensiveBar": 56 
+            }
+        }
     }
 }
