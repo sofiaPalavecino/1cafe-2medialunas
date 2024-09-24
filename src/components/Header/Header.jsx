8ee5e5ebee6bdc961ccae80d624580016787fc58
sofiaPalavecino <sofiaor29@gmail.com>
2024-09-24 12:51:01 -0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ export default function Header(){
                 <span>1 Café + 2 Medialunas</span>
             </div>
             <div className='header__links'>
-                <div><a href="">Barrios</a></div>
+                {/* <div><a href="">Barrios</a></div>
                 <div><a href="">Bares Notables</a></div>
-                <div><a href="">Universidades</a></div>
+                <div><a href="">Universidades</a></div> */}
                 <div><a href="">Créditos</a></div>
             </div>
         </header>
